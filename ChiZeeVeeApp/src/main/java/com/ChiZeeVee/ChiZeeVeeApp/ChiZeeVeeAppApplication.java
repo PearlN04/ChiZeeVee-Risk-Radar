dad342019f00cea39c7091d7e1583e06b2ac5cc1
nonts <nontsangaphelisa@gmail.com>
2025-05-20 14:12:21 +0200

    Upload capstone project

---
+++ ChiZeeVeeApp/src/main/java/com/ChiZeeVee/ChiZeeVeeApp/ChiZeeVeeAppApplication.java
@@ -0,0 +1,13 @@
+package com.ChiZeeVee.ChiZeeVeeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiZeeVeeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiZeeVeeAppApplication.class, args);
+	}
+
+}
---
+++ ChiZeeVeeApp/src/test/java/com/ChiZeeVee/ChiZeeVeeApp/ChiZeeVeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChiZeeVee.ChiZeeVeeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiZeeVeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
