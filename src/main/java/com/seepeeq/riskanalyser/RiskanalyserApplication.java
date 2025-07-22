9142d445c9f10e7f8c1745063e9375cc1c09031f
nonts <nontsangaphelisa@gmail.com>
2025-07-22 12:48:13 +0200

    Initial version

---
+++ src/main/java/com/seepeeq/riskanalyser/RiskanalyserApplication.java
@@ -0,0 +1,13 @@
+package com.seepeeq.riskanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskanalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskanalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seepeeq/riskanalyser/RiskanalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seepeeq.riskanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskanalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
