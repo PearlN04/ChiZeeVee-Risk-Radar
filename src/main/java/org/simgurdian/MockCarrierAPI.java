fcc96f5b1ec0713a8a955715196c3e33d162d82a
nonts <nontsangaphelisa@gmail.com>
2025-08-09 21:52:54 +0200

    Added mock carrier API code

--- src/main/java/org/simgurdian/MockCarrierAPI.java
+++ src/main/java/org/simgurdian/MockCarrierAPI.java
@@ -1,4 +1,18 @@
 package org.simgurdian;
 
+import java.util.Map;
+
 public class MockCarrierAPI {
+
+        // Simulate a database of phone numbers and their SIM ICCIDs
+        private static final Map<String, String> simDatabase = Map.of(
+                "+27731234567", "89450421180216254895", // Your SIM
+                "+27739876543", "89450421180216254321"  // Fraudster's SIM
+        );
+
+        //A network company's API checking SIM ICCID
+        public static String getCurrentIccid(String phoneNumber) {
+            return simDatabase.get(phoneNumber); // Returns null if number not found
+        }
+
 }
